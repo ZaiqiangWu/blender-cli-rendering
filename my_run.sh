61d9c1888c598eafdf3777d54d63fe666bf8836f
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-03-27 23:09:02 +0900

    add

---
+++ my_run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OUT_DIR="./out"
+
+RESOLUTION=100
+SAMPLINGS=128
+ANIM_FRAMES_OPTION="--render-anim"
+
+# Make this "true" when testing the scripts
+TEST=false
+if ${TEST}; then
+  RESOLUTION=10
+  SAMPLINGS=16
+  ANIM_FRAMES_OPTION="--render-frame 1..5"
+fi
+
+# Create the output directory
+mkdir -p ${OUT_DIR}
+
+# Run the scripts
+
+blender --background -noaudio --python ./10_mocap.py ${ANIM_FRAMES_OPTION} -- ./assets/motion/102_01.bvh ${OUT_DIR}/10/frame_ ${RESOLUTION} ${SAMPLINGS}
+blender --background -noaudio --python ./12_cloth.py ${ANIM_FRAMES_OPTION} -- ${OUT_DIR}/12/frame_ ${RESOLUTION} ${SAMPLINGS}
+
+
+# Perform ffmpeg for animations
+
+ffmpeg -y -r 24 -i ${OUT_DIR}/10/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/10_mocap.mp4
+ffmpeg -y -r 24 -i ${OUT_DIR}/12/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/12_cloth.mp4
