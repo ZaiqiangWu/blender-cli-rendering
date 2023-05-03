03be8d32832ad23c61f15a1c32ec54211f7c5a82
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-05-03 21:14:00 +0900

    add

--- my_run.sh
+++ my_run.sh
@@ -18,12 +18,12 @@ fi
 mkdir -p ${OUT_DIR}
 
 # Run the scripts
-blender --background -noaudio --python ./01_cube.py --render-frame 1 -- ${OUT_DIR}/01_cube_ ${RESOLUTION} ${SAMPLINGS}
-#blender --background -noaudio --python ./10_mocap.py ${ANIM_FRAMES_OPTION} -- ./assets/motion/102_01.bvh ${OUT_DIR}/10/frame_ ${RESOLUTION} ${SAMPLINGS}
-#blender --background -noaudio --python ./12_cloth.py ${ANIM_FRAMES_OPTION} -- ${OUT_DIR}/12/frame_ ${RESOLUTION} ${SAMPLINGS}
+
+blender --background -noaudio --python ./10_mocap.py ${ANIM_FRAMES_OPTION} -- ./assets/motion/102_01.bvh ${OUT_DIR}/10/frame_ ${RESOLUTION} ${SAMPLINGS}
+blender --background -noaudio --python ./12_cloth.py ${ANIM_FRAMES_OPTION} -- ${OUT_DIR}/12/frame_ ${RESOLUTION} ${SAMPLINGS}
 
 
 # Perform ffmpeg for animations
 
-#ffmpeg -y -r 24 -i ${OUT_DIR}/10/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/10_mocap.mp4
-#ffmpeg -y -r 24 -i ${OUT_DIR}/12/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/12_cloth.mp4
+ffmpeg -y -r 24 -i ${OUT_DIR}/10/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/10_mocap.mp4
+ffmpeg -y -r 24 -i ${OUT_DIR}/12/frame_%04d.png -pix_fmt yuv420p ${OUT_DIR}/12_cloth.mp4
